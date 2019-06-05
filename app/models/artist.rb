569757508504db05e3170e68fc2bef0a13a527f3
Yingqi Chen <chanwk18@hotmail.com>
2019-06-05 20:21:41 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -14,7 +14,7 @@ class Artist < ActiveRecord::Base
 
   def genre_count
     #return the number of genres associated with the artist
-    self.songs.collect{|s|s.genre}
+    self.songs.collect{|s|s.genre}.size
 
   end
 end
